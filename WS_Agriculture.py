9b023ae3a60571d978895e14b6112c8b3401313e
Srimathi-JMAN <srimathi.p@jmangroup.com>
2025-02-10 18:20:38 +0530

    created WS_Agriculture.py

---
+++ WS_Agriculture.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.agroinfomedia.com/"
+
+response = requests.get(url)
+
+soup= BeautifulSoup(response.text,"html.parser")
+
+names=soup.find('strong').get_text()
+
+print(names)
